41e2885377aa676f8a61287544e390d7fcc922e4
lollipopnougat <ab2defg145@gmail.com>
2020-03-11 19:43:02 +0800

    提交

---
+++ tcpserver.py
@@ -0,0 +1,53 @@
+"""
+server.py
+"""
+from socket import *
+from time import ctime
+import threading
+import time
+
+
+def server(ho, po):
+    HOST = ho
+    PORT = po
+    BUFSIZE = 4096
+    ADDR = (HOST, PORT)
+    tcpServer = socket(AF_INET, SOCK_STREAM)
+    tcpServer.bind(ADDR)
+    tcpServer.listen(10)
+
+    while True:
+        print('等待连接...')
+        tcpClient, addr = tcpServer.accept()
+        print(addr)
+
+        while True:
+            data = tcpClient.recv(BUFSIZE)
+            print(data.decode())
+            if not data:
+                print('---------')
+                break
+
+            buf = '[' + ctime() + ']' + data.decode()
+            tcpClient.send(buf.encode())
+
+        tcpClient.close()
+    tcpServer.close()
+
+
+def send_msg(string, host, port):
+    s = socket()
+    s.connect((host, port))
+    s.send(string.encode())
+    s.close()
+
+
+if __name__ == "__main__":
+    task = threading.Thread(target=server, args=('',13000,))
+    task.start()
+    ho = '127.0.0.1'
+    po = 13001
+    time.sleep(0.5)
+    while (True):
+        cmd = input('输入消息: ')
+        send_msg(cmd, ho, po)
